38a16d28a197a69a668af5ab3b465260fa1d2344
Suvetha B <suvetha.balakumar@sifycorp.com>
2024-05-26 21:13:13 +0530

    Create and run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -10,6 +11,7 @@ import Projects from "./pages/Projects";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
